eb61c2dca9fe4d56d6a07a9d55f4b296aaf745cc
Fernando Alvez <fernando.alvez@unam.edu.ar>
2022-12-01 12:51:45 -0300

    Form con custom hooks

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import CounterWithCustomHooks from './01-useState/CounterWithCustomHooks'
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+//import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   // </React.StrictMode>
 )
